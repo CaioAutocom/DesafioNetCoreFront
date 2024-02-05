a0ae980406701a7571dfaa260581f378b669fb39
Caio Autocom <caio.mendes@autocom-mg.com.br>
2024-02-05 20:48:41 -0300

    Adicionado página de Login

--- DesafioNetCoreFront/src/app/app.routes.ts
+++ DesafioNetCoreFront/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './users/pages/login-page/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        // quando a rota estiver vazia, redireciona para o login
+        path:'',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path:'login',
+        component: LoginPageComponent
+    }
+];
