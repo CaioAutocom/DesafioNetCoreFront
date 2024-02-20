a67aa81fbcfcbac6578ce098c87b73216b0e9ade
Caio Autocom <caio.mendes@autocom-mg.com.br>
2024-02-20 00:27:52 -0300

    iniciado criação dos forms para envio de post de login para api, pendente correção de bug de variável.

--- DesafioNetCoreFront/src/app/users/pages/login-page/login-page/login-page.component.ts
+++ DesafioNetCoreFront/src/app/users/pages/login-page/login-page/login-page.component.ts
@@ -1,30 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AuthService } from '../../../../services/auth.service';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
-  username: string = '';
-  password: string = '';
+  // username: string = '';
+  // password: string = '';
 
   constructor(private authService: AuthService){}
 
+  protected formBuilderService = inject(FormBuilder);
+  protected form = this.formBuilderService.group({
+    username: '',
+    password: ['']
+  })
   onSubmit()
   {
-    this.authService.login(this.username, this.password).subscribe(
+    const username = this.form.value.username;
+    const password = this.form.value.password;
+
+    this.authService.login(username, password).subscribe(
       response => 
       {
         this.authService.setToken(response.token);
         this.authService.setRefreshToken(response.refreshToken);
+        console.log(response.token);
       },
       error => { 
         // tratar'
       }
     )
+  }
+
+  protected printForm()
+  {
+    console.log(this.form);
   }
 }
 
